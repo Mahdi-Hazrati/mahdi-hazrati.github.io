3b94d45cb1f48e66937d33eaee2b949d26f4af7e
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-27 14:29:57 +0430

    add Introduction component

--- src/Components/Introduction/Introduction.jsx
+++ src/Components/Introduction/Introduction.jsx
@@ -1,8 +1,36 @@
 import "./introduction.scss"
+import ProfilePicture from "../../Assets/images/PngItem_503557.png"
+import { ArrowDownward } from "@mui/icons-material"
 
-const Introduction = ()=>{
+const Introduction = () => {
     return (
-        <div>Introduction</div>
+        <div className="introduction">
+            <div className="left">
+                <div className="wrapper">
+                    <h2 className="hello">Hello , My name is </h2>
+                    <h1 className="full-name">Mahdi Hazrati</h1>
+                    <div className="job-title">
+                        <div className="title-wrapper">
+                            <div className="title-item">Web-Developer</div>
+                            <div className="title-item">Writer</div>
+                            <div className="title-item">UI/UX-Designer</div>
+                            <div className="title-item">Story-Teller</div>
+                            <div className="title-item">Content-Creator</div>
+                        </div>
+                    </div>
+                    <p className="job-description">
+                        Collaborate with designers and developers to implement website designs and user interfaces
+                    </p>
+                </div>
+                <div className="scroll-down">
+                    <ArrowDownward className="icon" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="background"></div>
+                <img src={ProfilePicture} alt="mahdi hazrati profile" className="profile" />
+            </div>
+        </div>
     )
 }
 
